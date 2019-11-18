f80e1a39aa3f7b47ba2e0708542e8c99f3565938
Serjo <49415556+sergiomascarpone@users.noreply.github.com>
2019-11-18 21:35:51 +0300

    продолжение магаза

---
+++ NewTeacher 12.11.2019/online-shop/online-shop/scripts.js
@@ -0,0 +1,79 @@
+const button = document.querySelector('.menu-toggle');
+
+button.addEventListener('click', () => {
+    document.body.classList.toggle('menu-opened');
+});
+
+
+const products = [
+    {
+        name: 'Mayka',
+        size: 'M',
+        color: 'green',
+        img: './mayka.png',
+        price: '15'
+    },
+
+    {
+        name: 'Mayka',
+        size: 'S',
+        color: 'yelow',
+        img: './htani.png',
+        price: '25'
+    },
+
+    {
+        name: 'Mayka',
+        size: 'L',
+        color: 'black',
+        img: './mayka.png',
+        price: '35'
+    },
+];
+
+function showProducts(products) {
+
+    let view = '';
+
+    for (product of products) {
+        view += `
+<li class="product">
+<a class="product-link" href="#">
+    <div class="product-header">
+        <div class="product-size">size: ${product.size}</div>
+        <div class="product-color">color: ${product.color}</div>
+    </div>
+    <div class="product-content">
+        <img src="${product.img}" alt="mayka">
+    </div>
+
+    <div class="product-footer">
+        <div class="product-name"> ${product.name}</div>
+        <div class="product-price"> ${product.price}</div>
+    </div>
+</a>
+</li>`
+
+    }
+
+    document.querySelector('.products-conteiner').innerHTML = view;
+
+
+}
+
+function addProducts() {
+    product.push({
+        name: 'Htani',
+        size: 'S',
+        color: 'yelow',
+        img: './htani.png',
+        price: '25'
+    });
+    showProducts(products);
+}
+
+document.querySelector('#add-product').addEventListener('click', () => {
+    addProduct();
+});
+
+showProducts(products);
